968fe95c91df1111296a83da72a670cf9b85e9c2
Ali Azouz <132236484+Azouz8@users.noreply.github.com>
2024-09-09 02:55:00 +0300

    Solve Issue With CacheHelper

--- lib/features/settings/presentation/views/widgets/country_menu_widget.dart
+++ lib/features/settings/presentation/views/widgets/country_menu_widget.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news/core/cache_helper.dart';
-import 'package:news/features/settings/presentation/views/widgets/country_menu_item.dart';
 import 'package:news/features/home/presentation/manager/news_category_cubit/news_category_cubit.dart';
 import 'package:news/features/home/presentation/manager/top_headlines_cubit/top_headlines_cubit.dart';
+import 'package:news/features/settings/presentation/views/widgets/country_menu_item.dart';
 
 class CountryMenu extends StatelessWidget {
   CountryMenu({super.key});
 
   final countryController = TextEditingController();
-
+  var country = CacheHelper.getString(key: 'country');
   @override
   Widget build(BuildContext context) {
+    print(country);
     return DropdownMenu<CountryMenuItem>(
       textStyle: const TextStyle(fontSize: 20),
-      initialSelection: CountryMenuItem(country: CacheHelper.getString(key: 'country')!), // This sets Egypt as the initial selection
+      initialSelection:
+          CacheHelper.getString(key: 'country').toString() ==
+                  "us"
+              ? CountryMenuItem(country: "us")
+              : CountryMenuItem(country: "eg"),
       width: MediaQuery.sizeOf(context).width * 0.9,
       controller: countryController,
       label: const Text("Country"),
@@ -27,17 +32,20 @@ class CountryMenu extends StatelessWidget {
         ),
         DropdownMenuEntry(
           value: CountryMenuItem(country: 'us'),
-          labelWidget: const Text("United States", style: TextStyle(fontSize: 20)),
+          labelWidget:
+              const Text("United States", style: TextStyle(fontSize: 20)),
           leadingIcon: const Icon(Icons.flag),
           label: "United States",
         ),
       ],
       onSelected: (value) {
         CacheHelper.putString(key: "country", value: value!.country);
-        BlocProvider.of<TopHeadlinesCubit>(context).fetchTopHeadlines(country: CacheHelper.getString(key: 'country')!);
-        BlocProvider.of<NewsCategoryCubit>(context).fetchNewsCategory(country: CacheHelper.getString(key: 'country')!, category: 'Healthy');
+        BlocProvider.of<TopHeadlinesCubit>(context)
+            .fetchTopHeadlines(country: CacheHelper.getString(key: 'country')!);
+        BlocProvider.of<NewsCategoryCubit>(context).fetchNewsCategory(
+            country: CacheHelper.getString(key: 'country')!,
+            category: 'Healthy');
       },
-
     );
   }
 }
